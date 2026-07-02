6805a1560753c4e2b1747312a89be877f01f9448
Espen Holtebu <espen.holtebu@nav.no>
2026-07-02 13:00:44 +0200

    update pytest configuration

--- workflow.py
+++ workflow.py
@@ -2,20 +2,20 @@ import flyte
 
 
 env = flyte.TaskEnvironment(
-    name="addition-workflow",
-    image=flyte.Image.from_debian_base(python_version=(3, 12)),
+	name="addition-workflow",
+	image=flyte.Image.from_debian_base(python_version=(3, 12)),
 )
 
 
 def format_message(message: str) -> str:
-    return f"ok: {message}"
+	return f"ok: {message}"
 
 
 @env.task
 def hello() -> str:
-    return format_message("union")
+	return format_message("union")
 
 
 @env.task()
 async def main() -> str:
-    return hello()
+	return hello()
